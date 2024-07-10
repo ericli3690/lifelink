121ab65d7376744df5e35cd0cb2553dd7e373dfb
ericli3690 <ericli3690@gmail.com>
2024-07-10 13:01:09 -0600

    troubleshooting appbar

--- software/reactnative/lifelink/app/index.tsx
+++ software/reactnative/lifelink/app/index.tsx
@@ -1,10 +1,14 @@
+import { Component } from "@/components/Component";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View className="flex flex-col">
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Component />
+      </View>
     </SafeAreaView>
   );
 }
---
+++ software/reactnative/lifelink/components/Component.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+
+export function Component() {
+  return (
+    <View>
+      <Text>Testing testing!</Text>
+    </View>
+  )
+}
