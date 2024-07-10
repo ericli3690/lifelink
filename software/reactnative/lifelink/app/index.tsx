0c55546c28b0a23f39e2985b4ca1d03c46f92d8a
ericli3690 <ericli3690@gmail.com>
2024-07-10 15:29:20 -0600

    finally got app bar to work

--- software/reactnative/lifelink/app/index.tsx
+++ software/reactnative/lifelink/app/index.tsx
@@ -1,18 +1,22 @@
 import { Home } from "@/components/Home";
-import { StatusBar } from "expo-status-bar";
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { SvgUri } from "react-native-svg";
+import Suitcase from '@/assets/images/suitcase.svg';
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 
 export default function Index() {
   return (
     <>
-      <StatusBar backgroundColor="#FC4F42"></StatusBar>
-      <View className="flex flex-row bg-[#FC4F42] h-14 items-center p-4">
-        <Image className="size-8" source={require('@/assets/images/suitcase-medical-solid.svg')} />
-        {/* <Text className="text-white font-bold text-xl">LifeLink</Text> */}
-      </View>
+      <StatusBar backgroundColor="#FC4F42" />
       
+      <View className="flex flex-row bg-[#FC4F42] h-14 items-center p-4">
+        <Suitcase className="w-6 h-6 ml-2 mr-6" />
+        <Text className="text-white font-bold text-xl">LifeLink</Text>
+      </View>
+
       <SafeAreaView>
+        
         <Home />
       </SafeAreaView>
     </>
