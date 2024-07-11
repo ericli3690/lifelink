b4ddecfa612b1bfe88ca0e268c7dce37a32f8f64
ericli3690 <ericli3690@gmail.com>
2024-07-11 14:45:45 -0600

    integrated firebase, queries db

--- software/reactnative/lifelink/components/Home.tsx
+++ software/reactnative/lifelink/components/Home.tsx
@@ -1,7 +1,26 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import Map from "./Map";
+import { app } from '@/firebaseConfig.js';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { useEffect } from "react";
+import BackgroundTimer from 'react-native-background-timer';
 
 export function Home() {
+
+  useEffect(() => {
+    BackgroundTimer.runBackgroundTimer(() => { 
+      console.log("test")
+      }, 
+      3000);
+    (async () => {
+      const db = getFirestore(app);
+      const query = await getDocs(collection(db, "emergencies"));
+      query.forEach(document => {
+        console.log(document.data());
+      });
+    })();
+  }, []);
+
   return (
     <View className="mx-4">
       <Text className="text-xl text-stone-800 font-bold mb-2">Active Alerts</Text>
