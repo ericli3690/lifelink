99e9cc8532db490209e6a21667a0a11976391901
ericli3690 <ericli3690@gmail.com>
2024-07-10 19:34:41 -0600

    testing a webview solution

--- software/reactnative/lifelink/components/Home.tsx
+++ software/reactnative/lifelink/components/Home.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Linking, Alert } from "react-native";
 import Map from "./Map";
 
 export function Home() {
@@ -6,6 +6,15 @@ export function Home() {
     <View className="px-4">
       <Text>Testing testing!</Text>
       <Map />
+      {/* mandatory copyright attribution */}
+      <Text>Map data from <Text className="text-blue-400 font-bold" onPress={() => {
+        Linking.openURL('https://www.openstreetmap.org/copyright')
+          .catch(e => {
+            Alert.alert(
+              "An Error Occured."
+            )
+          })
+      }}>OpenStreetMap</Text></Text>
     </View>
   )
 }
--- software/reactnative/lifelink/components/Map.tsx
+++ software/reactnative/lifelink/components/Map.tsx
@@ -1,23 +1,8 @@
 import { Alert, Linking, Text, View } from "react-native";
-import '@/styles/leaflet.css';
-import '@/scripts/leaflet.js';
+import { WebView } from 'react-native-webview';
 
 export default function Map() {
   return (
-    <View>
-
-
-
-      {/* mandatory copyright attribution */}
-      <Text>Map data from <Text className="text-blue-400 font-bold" onPress={() => {
-        Linking.openURL('https://www.openstreetmap.org/copyright')
-          .catch(e => {
-            Alert.alert(
-              "An Error Occured."
-            )
-          })
-      }}>OpenStreetMap</Text></Text>
-
-    </View>
+    <WebView source={{ uri: 'https://reactnative.dev/' }} style={{ flex: 1 }} />
   )
 }
